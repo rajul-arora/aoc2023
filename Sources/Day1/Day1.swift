c5333c75e51b6ffac39eff4dda4d7616849f7184
Rajul Arora <rajularora1994@gmail.com>
2023-12-01 09:21:23 -0500

    Day 1 Part 1

--- Sources/Day1/Day1.swift
+++ Sources/Day1/Day1.swift
@@ -13,6 +13,28 @@ enum Day1Error: LocalizedError {
 
 struct Day1 {
     static func run(input: String) throws {
-        throw Day1Error.unknown
+        let lines = input.split(separator: "\n")
+        let result = lines.map { number(for: String($0)) }.reduce(0, +)
+        debugPrint(result)
+    }
+
+    static func number(for line: String) -> Int {
+        var firstNumberIndex: Int = -1
+        var secondNumberIndex: Int = -1
+        let split = Array(line)
+
+        split.enumerated().forEach { index, value in 
+            if let _ = Int(String(value)), firstNumberIndex == -1 {
+                firstNumberIndex = index
+            }
+        }
+
+        split.reversed().enumerated().forEach { index, value in 
+            if let _ = Int(String(value)), secondNumberIndex == -1 {
+                secondNumberIndex = split.count - index - 1
+            }
+        }
+          
+        return Int("\(split[firstNumberIndex])\(split[secondNumberIndex])") ?? 0
     }
 }
